28411ea3819a654f363e803e58897cb480f70610
Mahadev Chidanand Athani <athanimahadev348@gmail.com>
2024-04-29 15:27:23 +0530

    python day one

---
+++ day1/firstproject.py
@@ -0,0 +1,4 @@
+#Create a greeting for you program
+city_name=input("Enter you grew up city:")
+user_pet_name=input("Enter your pet name:")
+print(city_name+user_pet_name)
---
+++ day1/variabl.py
@@ -0,0 +1,12 @@
+'''name=input("Enter your name")
+print(len(name))'''
+
+a=41
+b=29
+print("a:",a)
+print("b:",b)
+b=a+b
+a=b-a
+b=b-a
+print(a)
+print(b)
