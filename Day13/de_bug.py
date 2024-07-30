43564e85329b958f2cee39e50ccc005c412493a1
Mahadev Chidanand Athani <athanimahadev348@gmail.com>
2024-07-31 00:46:56 +0530

    Day 13

---
+++ Day13/de_bug.py
@@ -0,0 +1,14 @@
+# def my_function():
+#     for i in range(20):
+#         if i==2:
+#             print("You got it")
+#             break
+# my_function()
+
+from random import randint
+
+dice_imgs=[0,1,2,3,4,5  ]
+
+dice_num=6 #randint(1,6)
+
+print(dice_imgs[dice_num])
